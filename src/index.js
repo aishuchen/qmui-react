706e3ca51ed2a772418fc07e5d65b96cb5f568db
huoyue.jiang <huoyue.jiang@mail.hypers.com>
2021-11-14 22:21:41 +0800

    update: update

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+let memberIP = ""
+
+// 直接创建
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
